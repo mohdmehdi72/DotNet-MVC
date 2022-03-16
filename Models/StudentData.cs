130a925e8aa9eb4bc8294cf78f1340e583776703
Mohd Mehdi <77981548+mohdmehdi72@users.noreply.github.com>
2022-03-17 01:13:01 +0530

    Add files via upload
    CURD Application

---
+++ Models/StudentData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class StudentData
+    {
+        public int empid { get; set; }
+        [Required]
+        public string ename { get; set; }
+        [Required]
+        public Nullable<decimal> mobile { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string city { get; set; }
+    }
+}
